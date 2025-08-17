6312e1a03a69e926d4c3b18d7634a53880254c29
Abhijith V <abhivishnu060@gmail.com>
2025-08-17 09:50:32 +0000

    next config update

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,12 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'example.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'tastyspoon.s3.ap-south-1.amazonaws.com',
         port: '',
         pathname: '/**',
